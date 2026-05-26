9f65b583328de3abb2c4e94490adca5100bf9a73
Mykyta <barduk234@gmail.com>
2026-05-26 15:02:31 +0300

    fix login 2

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { api } from './api';
+import { api } from '../api';
 import { Mail, ShieldAlert, Lock } from 'lucide-react';
 
 interface LoginProps {
